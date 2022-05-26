09cc3f73aab89e59f5ce40e7eff01dfc514f2cee
ibnshayed <ibnshayed@gmail.com>
2022-05-26 13:13:54 +0600

    login page added

--- components/headers/Header.js
+++ components/headers/Header.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Sidemenu from "../sidemenus/Sidemenu.js";
 import  Link from 'next/link';
+import BraveBrowser from "../svgs/BraveBrowser.js";
 
 const Header = () => {
   const [sidebar, setSideBar] = useState(false);
@@ -9,24 +10,27 @@ const Header = () => {
     <>
       <div className={"bg-blue-500 flex justify-between items-center"}>
         <div className={"p-4 text-white font-bold text-2xl ml-0 md:ml-20"}>
-          Next.js
+          <BraveBrowser fill="white"/>
         </div>
         <div
           className={
             "p-4 hidden md:flex justify-between items-center text-white space-x-4 mr-0 md:mr-20"
           }
         >
-          <Link href="/login" className={"px-3 py-2"}>
-            <a>Login</a>
+          <Link href="/login" >
+            <a className={"px-3 py-2 hover:underline transition-all duration-500 ease-in-out hover:-translate-y-1"}>Login</a>
           </Link>
-          <Link href="/signup" className={"px-3 py-2"}>
-					<a>Signup</a>
-          </Link>
+          <Link href="/signup" >
+					<a className={"px-3 py-2 hover:underline transition-all duration-500 ease-in-out hover:-translate-y-1"}>Signup</a>
+					</Link>
+					<div className={"flex items-center text-black bg-blue-300 px-2 py-1 rounded-full space-x-2"}>
+						<p>Emran Ibn Shayed</p>
           <img
             src="https://images.generated.photos/vyaPXjHlqtBnCOz2zGuPFcbo4u9oGehWdZi9jBe1uhU/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/Njk2NTgxLmpwZw.jpg"
             alt="profile"
             className="inline rounded-full w-10 h-10 shadow-md"
           />
+					</div>
         </div>
         <div
           className={"p-4 block md:hidden cursor-pointer"}
